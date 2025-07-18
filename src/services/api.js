6ba0f210c3f7fcbde568d089322e773b3c564cb7
rafaelsnts <santosrafaelcontato@gmail.com>
2025-07-18 13:20:45 -0300

    fix: corrigir baseURL da API para incluir /api

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,10 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${API_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
